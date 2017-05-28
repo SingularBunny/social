bfe412aeb7f58095a215512c895a9a495fe8a016
Eugeny Lopatkin <elopatkin@thumbtack.net>
2017-05-28 08:14:16 +0400

    multiprocessing logger implementation

--- logging_utils/__init__.py
+++ logging_utils/__init__.py
@@ -1,10 +1,28 @@
+"""
+
+To use multiprocessing loger main process should something like this:
+>>> q = Queue()
+>>> with open(LOGGER_CONFIG, 'r') as logger_config:
+>>>     config = load(logger_config, Loader=Loader)
+>>>     config_worker = config.get('worker')  # logger config for workers.
+>>>     config_worker['queue'] = q
+>>>     config_listener = config.get('listener')
+>>> stop_event = Event()
+>>> lp = Process(target=listener_process, name='listener',
+>>>              args=(q, stop_event, config_listener))
+>>> lp.start()
+>>> stop_event.set()
+>>> lp.join()
+and inside spawned processes:
+>>> logging.config.dictConfig(config)
+>>> logger = logging.getLogger('ProcessName')
+>>> logger.debug('message')
+"""
+
 import logging
 import logging.config
 import logging.handlers
 from multiprocessing import Process, Queue, Event, current_process
-import os
-import random
-import time
 
 from logging_utils import handlers
 
@@ -14,9 +32,7 @@ except ImportError:
     from yaml import Loader, Dumper, load
 
 # config paths
-INITIAL_CONFIG = 'initial_config'
-WORKER_CONFIG = 'worker_config'
-LISTENER_CONFIG = 'listener_config'
+LOGGER_CONFIG = 'logger_config.yaml'
 
 class SimpleHandler:
     """
@@ -44,90 +60,5 @@ def listener_process(q, stop_event, config):
     logging.config.dictConfig(config)
     listener = handlers.QueueListener(q, SimpleHandler())
     listener.start()
-    if os.name == 'posix':
-        # On POSIX, the setup logger will have been configured in the
-        # parent process, but should have been disabled following the
-        # dictConfig call.
-        # On Windows, since fork isn't used, the setup logger won't
-        # exist in the child, so it would be created and the message
-        # would appear - hence the "if posix" clause.
-        logger = logging.getLogger('setup')
-        logger.critical('Should not appear, because of disabled logger ...')
     stop_event.wait()
     listener.stop()
-
-def worker_process(config):
-    """
-    A number of these are spawned for the purpose of illustration. In
-    practice, they could be a heterogeneous bunch of processes rather than
-    ones which are identical to each other.
-
-    This initialises logging according to the specified configuration,
-    and logs a hundred messages with random levels to randomly selected
-    loggers.
-
-    A small sleep is added to allow other processes a chance to run. This
-    is not strictly needed, but it mixes the output from the different
-    processes a bit more than if it's left out.
-    """
-    logging.config.dictConfig(config)
-    levels = [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR,
-              logging.CRITICAL]
-    loggers = ['foo', 'foo.bar', 'foo.bar.baz',
-               'spam', 'spam.ham', 'spam.ham.eggs']
-    if os.name == 'posix':
-        # On POSIX, the setup logger will have been configured in the
-        # parent process, but should have been disabled following the
-        # dictConfig call.
-        # On Windows, since fork isn't used, the setup logger won't
-        # exist in the child, so it would be created and the message
-        # would appear - hence the "if posix" clause.
-        logger = logging.getLogger('setup')
-        logger.critical('Should not appear, because of disabled logger ...')
-    for i in range(100):
-        lvl = random.choice(levels)
-        logger = logging.getLogger(random.choice(loggers))
-        logger.log(lvl, 'Message no. %d', i)
-        time.sleep(0.01)
-
-def main():
-    q = Queue()
-
-    with open(INITIAL_CONFIG, 'r') as initial_config:
-        with open(WORKER_CONFIG, 'r') as worker_config:
-            with open(LISTENER_CONFIG, 'r') as listener_config:
-                config_initial = load(initial_config, Loader=Loader)
-                config_worker = load(worker_config, Loader=Loader)
-                config_worker['queue'] = q
-                config_listener = load(listener_config, Loader=Loader)
-
-    # Log some initial events, just to show that logging in the parent works
-    # normally.
-    logging.config.dictConfig(config_initial)
-    logger = logging.getLogger('setup')
-    logger.info('About to create workers ...')
-    workers = []
-    for i in range(5):
-        wp = Process(target=worker_process, name='worker %d' % (i + 1),
-                     args=(config_worker,))
-        workers.append(wp)
-        wp.start()
-        logger.info('Started worker: %s', wp.name)
-    logger.info('About to create listener ...')
-    stop_event = Event()
-    lp = Process(target=listener_process, name='listener',
-                 args=(q, stop_event, config_listener))
-    lp.start()
-    logger.info('Started listener')
-    # We now hang around for the workers to finish their work.
-    for wp in workers:
-        wp.join()
-    # Workers all done, listening can now stop.
-    # Logging in the parent still works normally.
-    logger.info('Telling listener to stop ...')
-    stop_event.set()
-    lp.join()
-    logger.info('All done.')
-
-if __name__ == '__main__':
-    main()
